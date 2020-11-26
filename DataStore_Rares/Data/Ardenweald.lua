9a18e2f2fd6e6266ff9141e194ecb5db107adfd1
teelolws <49792789+teelolws@users.noreply.github.com>
2020-11-26 17:29:33 +1300

    Add some Shadowlands rares - work in progress

---
+++ DataStore_Rares/Data/Ardenweald.lua
@@ -0,0 +1,27 @@
+local addonName = "DataStore_Rares"
+local addon = _G[addonName]	
+
+addon.RareList.Ardenweald = {
+    "Ardenweald",
+	{
+        -- "Creature ID", "Quest ID", "Alliance Quest ID", "Horde Quest ID", "English Name"
+		{ 164477, 59226, nil, nil, "Deathbinder Hroth"},
+		{ 164238, nil, 59201, 62271, "Deifir the Untamed"},
+		{ 163229, 58987, nil, nil, "Dustbrawl"},
+		{ 167851, 60266, nil, nil, "Egg-Tender Leh'go"},
+		{ 171688, 61184, nil, nil, "Faeflayer"},
+		{ 163370, 59006, nil, nil, "Gormbore"},
+		{ 164107, 59145, nil, nil, "Gormtamer Tizo"},
+		{ 164112, 59157, nil, nil, "Humon'gozz"},
+		{ 160448, 59221, nil, nil, "Hunter Vivian"},
+		{ 165053, 59431, nil, nil, "Mymaen"},
+		{ 164547, 59235, nil, nil, "Mystic Rainbowhorn"},
+		{ 164391, nil, 59208, 62270, "Old Ardeite"},
+		{ 167726, 60273, nil, nil, "Rootwrithe"},
+		{ 167724, 60258, nil, nil, "Rotbriar Changeling"},
+		{ 171451, 61177, nil, nil, "Soultwister Cero"},
+		{ 167721, 60290, nil, nil, "The Slumbering Emperor"},
+		{ 164147, 59170, nil, nil, "Wrigglemortis"},
+	},
+    "DAILY",
+}
---
+++ DataStore_Rares/Data/Bastion.lua
@@ -0,0 +1,22 @@
+local addonName = "DataStore_Rares"
+local addon = _G[addonName]	
+
+addon.RareList.Bastion = {
+    "Bastion",
+	{
+        -- "Creature ID", "Quest ID", "Alliance Quest ID", "Horde Quest ID", "English Name"
+		{ 171211, 61083, nil, nil, "Aspirant Eolis"},
+		{ 171189, 59022, nil, nil, "Bookkeeper Mnemis"},
+		{ 171014, 61002, nil, nil, "Collector Astorestes"},
+		{ 171010, 60999, nil, nil, "Corrupted Clawguard"},
+		{ 170623, 60883, nil, nil, "Dark Watcher"},
+		{ 163460, 62650, nil, nil, "Dionae"},
+		{ 171009, 60998, nil, nil, "Enforcer Aegeon"},
+		{ 160721, 58222, nil, nil, "Fallen Acolyte Erisne"},
+		{ 160882, 58319, nil, nil, "Nikara Blackheart"},
+		{ 160985, 58320, nil, nil, "Selena the Reborn"},
+		{ 170899, 60977, nil, nil, "The Ascended Council"},
+		{ 171008, 60997, nil, nil, "Unstable Memory"},
+	},
+    "DAILY",
+}
